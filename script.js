f901922338ef2c148c29081ddf538ae0bdc93a34
Ferid Mirzeyev <feridmrzyv2005@gmail.com>
2025-05-09 18:59:12 +0400

    module-3-update

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  });
+});
